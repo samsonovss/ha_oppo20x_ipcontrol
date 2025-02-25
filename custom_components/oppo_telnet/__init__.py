d3f41c6903432bbe55df051adfd22e378dc4d811
Anton <41430933+samsonovss@users.noreply.github.com>
2025-02-25 21:11:18 +0500

    Update __init__.py

--- custom_components/oppo_telnet/__init__.py
+++ custom_components/oppo_telnet/__init__.py
@@ -1,8 +1,19 @@
 """Oppo Telnet integration for Home Assistant."""
-from homeassistant.helpers import entity_registry as er
+import voluptuous as vol
+from homeassistant.const import CONF_HOST
+from homeassistant.helpers import config_validation as cv
 from .media_player import OppoTelnetMediaPlayer
 
 DOMAIN = "oppo_telnet"
+
+CONFIG_SCHEMA = vol.Schema({
+    DOMAIN: vol.All(
+        cv.ensure_list,
+        [vol.Schema({
+            vol.Required(CONF_HOST): cv.string,
+        })]
+    )
+}, extra=vol.ALLOW_EXTRA)
 
 def setup(hass, config):
     """Set up the Oppo Telnet component via YAML."""
@@ -10,11 +21,8 @@ def setup(hass, config):
         return True
 
     for conf in config[DOMAIN]:
-        host = conf.get("host")
-        if host:
-            player = OppoTelnetMediaPlayer(host)
-            hass.data.setdefault(DOMAIN, []).append(player)
-            hass.helpers.discovery.load_platform(
-                "media_player", DOMAIN, {"host": host}, config
-            )
+        host = conf[CONF_HOST]
+        hass.helpers.discovery.load_platform(
+            "media_player", DOMAIN, {"host": host}, config
+        )
     return True
